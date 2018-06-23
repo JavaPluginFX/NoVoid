dc3d09f2c4c6a3694e0b4d14be22ba5d042fa389
EinfachNurNils <39995171+JavaPluginFX@users.noreply.github.com>
2018-06-23 12:34:49 +0200

    Create Main.php

---
+++ src/Void/Main.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Void\NoVoid;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\level\Position;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\entity\Entity;
+use pocketmine\math\Vector3;
+use pocketmine\event\player\PlayerMoveEvent;
+    
+class Main extends PluginBase implements Listener{
+    
+public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function checkVoid(PlayerMoveEvent $event){
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ();
+    $level = $this->getServer()->getDefaultLevel();
+        if($event->getTo()->getFloorY() < 0){
+            switch(mt_rand(1, 2) == 1){
+              case 1:
+              $player->teleport(new Position($x, $y, $z, $level));
+              $player->setHealth($player->getHealth(20));
+              $player->sendMessage("§d§l»§r§aDu wurdest wieder zur §bLobby§a teleportiert§d§l«");
+              break;
+              case 2:
+              break;
+           }
+       }
+    }
+}
